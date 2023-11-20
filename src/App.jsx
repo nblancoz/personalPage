8ae3df058e6674c7b0a6615aefdc4e7898d00b4a
nblancoz <nblancozappala@gmail.com>
2023-11-20 23:07:17 +0100

    added contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./components/About/About.jsx";
 import Biography from "./components/Biography/Biography.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 import Header from "./components/Header/Header.jsx";
 import Portfolio from "./components/Portfolio/Portfolio.jsx";
 
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Biography />
       <Portfolio />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact:</h1>
+      <section id="images">
+        <img id="gmail" src="../assets/Gmail-logo.png" alt="" />
+        <img id="link" src="../assets/LinkedIn-logo.webp" alt="" />
+      </section>
+    </div>
+  );
+}
+
+export default Contact;
